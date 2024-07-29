2b85ad8afc6f669048ab17f7a91def801e32f050
Jean <evgenijpocekutov6@gmail.com>
2024-07-29 16:28:27 +0700

    next version 3

--- AutoFilling.cs
+++ AutoFilling.cs
@@ -11,37 +11,44 @@ namespace LibraryForPersonnelManagement
         {
 
         }
-
         public void AutoFillingProgram()
         {
-            ConsoleKeyInfo cki = new ConsoleKeyInfo();
-            ConsoleKeyInfo emptyInfo = new ConsoleKeyInfo();
+            string? key = "";
 
             Task.Run(() => {
-                cki = Console.ReadKey(true);
+                while (key != "`")
+                {
+                    key = Console.ReadLine();
+                }
             });
 
+            Console.WriteLine("\n\t\u001b[31mTap to start any button");
+            Console.ReadLine();
             Random rand = new Random();
 
-            while (cki.Key != ConsoleKey.R)
+            while (key != "`")
             {
                 int numberCompany = rand.Next(0, 20);
-                string[] nameCompany = File.ReadAllLines("..\\..\\..\\Naming\\CompanyName.txt")[numberCompany].Split(' ');
-                Company company = new Company(nameCompany[1]);
+                string nameCompany = File.ReadAllLines("..\\..\\..\\Naming\\CompanyName.txt")[numberCompany];
+                Company company = new Company(nameCompany.Substring(4, nameCompany.Length - 4));
 
-                Console.WriteLine($"\n\tNew Company has been created: {company.Name}");
+                Console.WriteLine($"\n\t\u001b[31mNew Company has been created:\u001b[0m {company.Name}");
 
-                while (cki.Key != ConsoleKey.E)
+                Thread.Sleep(1000);
+
+                while (key.ToLower() != "c" && key != "`")
                 {
                     int numberDep = rand.Next(0, 11);
-                    string[] nameDepartment = File.ReadAllLines("..\\..\\..\\Naming\\DepartmentName.txt")[numberDep].Split(' ');
-                    Department department = new Department(nameDepartment[1]);
+                    string nameDepartment = File.ReadAllLines("..\\..\\..\\Naming\\DepartmentName.txt")[numberDep];
+                    Department department = new Department(nameDepartment.Substring(4, nameDepartment.Length - 4));
 
                     company.Departments.Add(department);
-                    Console.WriteLine($"\t\tDepartment has been added: {department.Name}");
+                    Console.WriteLine($"\t  \u001b[32mDepartment has been added:\u001b[0m {department.Name}");
+
+                    Thread.Sleep(1000);
 
                     int i = 0;
-                    while (cki.Key != ConsoleKey.W)
+                    while (key.ToLower() != "d" && key.ToLower() != "c" && key != "`")
                     {
                         i++;
                         int numberNameManager = rand.Next(0, 50);
@@ -52,13 +59,15 @@ namespace LibraryForPersonnelManagement
                         Manager manager = new Manager(i, nameManager[1], surnameManager[1], ageManager, "Manager", salaryManager, department.Name);
 
                         department.Managers.Add(manager);
-                        Console.WriteLine($"\t\tManager has been hired: {manager.FirstName} {manager.LastName}");
+                        Console.WriteLine($"\t    \u001b[33mManager has been hired:\u001b[0m {manager.FirstName} {manager.LastName}");
 
-                        while(cki.Key != ConsoleKey.Q)
+                        Thread.Sleep(1000);
+
+                        while (key.ToLower() != "m" && key.ToLower() != "d" && key.ToLower() != "c" && key != "`")
                         {
                             i++;
                             int numberPos = rand.Next(0, 55);
-                            string[] position = File.ReadAllLines("..\\..\\..\\Naming\\Position.txt")[numberPos].Split(' ');
+                            string position = File.ReadAllLines("..\\..\\..\\Naming\\Position.txt")[numberPos];
                             int numberNameEmployee = rand.Next(0, 50);
                             string[] nameEmployee = File.ReadAllLines("..\\..\\..\\Naming\\FirstName.txt")[numberNameEmployee].Split(' ');
                             string[] surnameEmployee = File.ReadAllLines("..\\..\\..\\Naming\\LastName.txt")[numberNameEmployee].Split(' ');
@@ -67,45 +76,51 @@ namespace LibraryForPersonnelManagement
                             if (position.Contains("Sales"))
                             {
                                 decimal salesTarget = rand.Next(1000000, 4000000);
-                                Salesperson salesperson = new Salesperson(i, nameEmployee[1], surnameEmployee[1], ageEmployee, position[1], salaryEmployee, department.Name, salesTarget, 0.8m * salesTarget);
+                                Salesperson salesperson = new Salesperson(i, nameEmployee[1], surnameEmployee[1], ageEmployee, position.Substring(4, position.Length - 4), salaryEmployee, department.Name, salesTarget, 0.8m * salesTarget);
                                 company.Employees.Add(salesperson);
                                 department.Employees.Add(salesperson);
                                 manager.Teams.Add(salesperson);
-                                Console.WriteLine($"\t\tSalesperson has been hired for the position {salesperson.Position}: {salesperson.FirstName} {salesperson.LastName}");
+                                Console.WriteLine($"\t\t\u001b[34mSalesperson has been hired for the position\u001b[0m {salesperson.Position}: {salesperson.FirstName} {salesperson.LastName}");
                             }
                             else if (position.Contains("Developer"))
                             {
                                 int numberLanguage = rand.Next(0, 20);
                                 string[] language = File.ReadAllLines("..\\..\\..\\Naming\\TechnologyName.txt")[numberLanguage].Split(' ');
                                 int skillLevel = rand.Next(0, 6);
-                                Developer developer = new Developer(i, nameEmployee[1], surnameEmployee[1], ageEmployee, position[1], salaryEmployee, department.Name, language[1], skillLevel);
+                                Developer developer = new Developer(i, nameEmployee[1], surnameEmployee[1], ageEmployee, position.Substring(4, position.Length - 4), salaryEmployee, department.Name, language[1], skillLevel);
                                 company.Employees.Add(developer);
                                 department.Employees.Add(developer);
                                 manager.Teams.Add(developer);
-                                Console.WriteLine($"\t\tDeveloper has been hired for the position {developer.Position}: {developer.FirstName} {developer.LastName}");
+                                Console.WriteLine($"\t\t\u001b[35mDeveloper has been hired for the position\u001b[0m {developer.Position}: {developer.FirstName} {developer.LastName}");
                             }
                             else
                             {
-                                Employee employee = new Employee(i, nameEmployee[1], surnameEmployee[1], ageEmployee, position[1], salaryEmployee, department.Name);
+                                Employee employee = new Employee(i, nameEmployee[1], surnameEmployee[1], ageEmployee, position.Substring(4, position.Length - 4), salaryEmployee, department.Name);
                                 company.Employees.Add(employee);
                                 department.Employees.Add(employee);
                                 manager.Teams.Add(employee);
-                                Console.WriteLine($"\t\tEmployee has been hired for the position {employee.Position}: {employee.FirstName} {employee.LastName}");
+                                Console.WriteLine($"\t\t\u001b[36mEmployee has been hired for the position\u001b[0m {employee.Position}: {employee.FirstName} {employee.LastName}");
                             }
-
-
-                            cki = emptyInfo;
+                            key = "";
                             Thread.Sleep(1000);
                         }
-                        cki = emptyInfo;
+                        if (key.ToLower() == "d" || key.ToLower() == "c" || key == "`")
+                            break;
+                        key = "";
                         Thread.Sleep(1000);
                     }
-                    cki = emptyInfo;
+                    if (key.ToLower() == "c" || key == "`")
+                        break;
+                    key = "";
                     Thread.Sleep(1000);
                 }
-                cki = emptyInfo;
+                if (key.ToLower() == "`")
+                    break;
+                key = "";
                 Thread.Sleep(1000);
             }
+
+            Console.WriteLine("\n\t\u001b[31mAll Objects in this World have been filled in using Autofill\u001b[0m");
         }
       
     }
--- obj/Debug/net8.0/LibraryForPersonnelManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryForPersonnelManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryForPersonnelManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b53ca6a00846bb609b79d7d3dfb662b7bf2ce256")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4db0b989e19be3ef824ed2c49e9b3bae8cb6b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryForPersonnelManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryForPersonnelManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
